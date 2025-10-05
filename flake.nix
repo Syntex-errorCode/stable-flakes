62c0c8b48ac25d2017a19a7518fec39bcf1e0716
Else, Someone <else@someonex.net>
2025-10-06 00:18:17 +0300

    trigger flakehub action

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = ''
-      A very basic (and stable) flake.
-      Make your Flakes stable with just one input!
-      Reproducibility issues in libfetchers solved, once and for all!
+    A very basic (and stable) flake.
+    Make your Flakes stable with just one input!
+    Reproducibility issues in libfetchers solved, once and for all!
   '';
   inputs = { };
   outputs =
@@ -50,6 +50,7 @@
           "premium"
           "STEERING-COMMITTEE-IS-WATCHING-YOU"
 
+          "flakehub"
           "nixos-jetson"
           "cuda"
           "use-more-s3-space"
@@ -108,39 +109,37 @@
         error = throw self.lib.md;
 
         # :: String -> a -> a
-        warn =
-          (builtins.warn or (msg: builtins.trace "\nEVALUATION WARNING:\n${msg}"))
-            self.lib.md;
+        warn = (builtins.warn or (msg: builtins.trace "\nEVALUATION WARNING:\n${msg}")) self.lib.md;
 
         # :: String
         txt = (import ./flake.nix).description;
 
         # :: String
         md = ''
-    Flakes are now Stable!
-    ======================
+          Flakes are now Stable!
+          ======================
 
-    With the recent Feature Freeze announcement[^1] and with Robert H
-    stepping away from the NixOS/Nix Team, Flakes are, after an almost
-    decade-long stalement, finally recognized as Stable by virtuall all
-    vested parties. This means we can finally move on, and begin to isolate
-    and remove the hacks and the vast mass of technical debt that has been
-    supporting the legacy interface, preventing progress, and spoiling the
-    UX. This is the end of Drama. We may now start addressing the
-    long-standing usability issues, such as the support for Dependency
-    Injection, proper Versioning, and easier composition of Out-of-Tree
-    Projects. The first step, of course, is to delete your `flake.nix` and
-    switch to a proper lockfile management solution[^2][^3].
-    Do not forget the prior art on the subject![^4][^5][^6][^7]
+          With the recent Feature Freeze announcement[^1] and with Robert H
+          stepping away from the NixOS/Nix Team, Flakes are, after an almost
+          decade-long stalement, finally recognized as Stable by virtuall all
+          vested parties. This means we can finally move on, and begin to isolate
+          and remove the hacks and the vast mass of technical debt that has been
+          supporting the legacy interface, preventing progress, and spoiling the
+          UX. This is the end of Drama. We may now start addressing the
+          long-standing usability issues, such as the support for Dependency
+          Injection, proper Versioning, and easier composition of Out-of-Tree
+          Projects. The first step, of course, is to delete your `flake.nix` and
+          switch to a proper lockfile management solution[^2][^3].
+          Do not forget the prior art on the subject![^4][^5][^6][^7]
 
 
-    [^1]: https://wiki.lix.systems/books/lix-contributors/page/flakes-feature-freeze
-    [^2]: https://github.com/nikstur/lon
-    [^3]: https://github.com/andir/npins
-    [^4]: https://jade.fyi/blog/flakes-arent-real
-    [^5]: https://github.com/infinisil/sanix
-    [^6]: https://piegames.de/dumps/pinning-nixos-with-npins-revisited
-    [^7]: https://piegames.de/dumps/nixpkgs-global-overlays
+          [^1]: https://wiki.lix.systems/books/lix-contributors/page/flakes-feature-freeze
+          [^2]: https://github.com/nikstur/lon
+          [^3]: https://github.com/andir/npins
+          [^4]: https://jade.fyi/blog/flakes-arent-real
+          [^5]: https://github.com/infinisil/sanix
+          [^6]: https://piegames.de/dumps/pinning-nixos-with-npins-revisited
+          [^7]: https://piegames.de/dumps/nixpkgs-global-overlays
         '';
 
       };
