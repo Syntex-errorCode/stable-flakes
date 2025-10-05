9d928efac45fe5c5f2acb0c9d0e821fdd3359962
Else, Someone <else@someonex.net>
2025-10-05 23:57:48 +0300

    flake: reuse the notice for description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,30 @@
 {
-  description = "A very basic (and stable) flake";
+  description = ''
+    Flakes are now Stable!
+    ======================
+
+    With the recent Feature Freeze announcement[^1] and with Robert H
+    stepping away from the NixOS/Nix Team, Flakes are, after an almost
+    decade-long stalement, finally recognized as Stable by virtuall all
+    vested parties. This means we can finally move on, and begin to isolate
+    and remove the hacks and the vast mass of technical debt that has been
+    supporting the legacy interface, preventing progress, and spoiling the
+    UX. This is the end of Drama. We may now start addressing the
+    long-standing usability issues, such as the support for Dependency
+    Injection, proper Versioning, and easier composition of Out-of-Tree
+    Projects. The first step, of course, is to delete your `flake.nix` and
+    switch to a proper lockfile management solution[^2][^3].
+    Do not forget the prior art on the subject![^4][^5][^6][^7]
+
+
+    [^1]: https://wiki.lix.systems/books/lix-contributors/page/flakes-feature-freeze
+    [^2]: https://github.com/nikstur/lon
+    [^3]: https://github.com/andir/npins
+    [^4]: https://jade.fyi/blog/flakes-arent-real
+    [^5]: https://github.com/infinisil/sanix
+    [^6]: https://piegames.de/dumps/pinning-nixos-with-npins-revisited
+    [^7]: https://piegames.de/dumps/nixpkgs-global-overlays
+  '';
   inputs = { };
   outputs =
     { self, ... }:
@@ -107,32 +132,7 @@
         warn = (builtins.warn or (msg: builtins.trace "EVALUATION WARNING:\n${msg}")) self.lib.announcement;
 
         # :: String
-        announcement = ''
-          Flakes are now Stable!
-          ======================
-
-          With the recent Feature Freeze announcement[^1] and with Robert H
-          stepping away from the NixOS/Nix Team, Flakes are, after an almost
-          decade-long stalement, finally recognized as Stable by virtuall all
-          vested parties. This means we can finally move on, and begin to isolate
-          and remove the hacks and the vast mass of technical debt that has been
-          supporting the legacy interface, preventing progress, and spoiling the
-          UX. This is the end of Drama. We may now start addressing the
-          long-standing usability issues, such as the support for Dependency
-          Injection, proper Versioning, and easier composition of Out-of-Tree
-          Projects. The first step, of course, is to delete your `flake.nix` and
-          switch to a proper lockfile management solution[^2][^3].
-          Do not forget the prior art on the subject![^4][^5][^6][^7]
-
-
-          [^1]: https://wiki.lix.systems/books/lix-contributors/page/flakes-feature-freeze
-          [^2]: https://github.com/nikstur/lon
-          [^3]: https://github.com/andir/npins
-          [^4]: https://jade.fyi/blog/flakes-arent-real
-          [^5]: https://github.com/infinisil/sanix
-          [^6]: https://piegames.de/dumps/pinning-nixos-with-npins-revisited
-          [^7]: https://piegames.de/dumps/nixpkgs-global-overlays
-        '';
+        announcement = (import ./flake.nix).description;
 
       };
       nixosModules.default = self.lib.error;
@@ -141,7 +141,7 @@
 
       checks = self.lib.forEachSystem (
         system:
-        self.lib.warn {
+        /* self.lib.warn */ {
         }
       );
       packages = self.lib.warn (
