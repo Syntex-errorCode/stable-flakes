cea4946061afe52e45773e7d6a93f8d50277d56e
Else, Someone <else@someonex.net>
2025-10-06 03:55:04 +0300

    fix: typos; reindent

--- flake.nix
+++ flake.nix
@@ -123,16 +123,17 @@
 
           With the recent Feature Freeze announcement[^1] and with Robert H
           stepping away from the NixOS/Nix Team, Flakes are, after an almost
-          decade-long stalement, finally recognized as Stable by virtuall all
-          vested parties. This means we can finally move on, and begin to isolate
-          and remove the hacks and the vast mass of technical debt that has been
-          supporting the legacy interface, preventing progress, and spoiling the
-          UX. This is the end of Drama. We may now start addressing the
-          long-standing usability issues, such as the support for Dependency
-          Injection, proper Versioning, and easier composition of Out-of-Tree
-          Projects. The first step, of course, is to delete your `flake.nix` and
-          switch to a proper lockfile management solution[^2][^3].
-          Do not forget the prior art on the subject![^4][^5][^6][^7]
+          decade-long period of stagnation, finally recognized as Stable by
+          virtually all vested parties. This means we can finally move on, and
+          begin to isolate and remove the hacks and the vast mass of technical
+          debt that has been supporting the legacy interface, preventing
+          progress, and damaging the UX. This is the end of Drama. We may now
+          start addressing the long-standing usability issues, such as the
+          support for Dependency Injection, proper Versioning, and easier
+          composition of Out-of-Tree Projects. The first step, of course, is to
+          delete your `flake.nix` and switch to a proper lockfile management
+          solution[^2][^3]. Do not forget the prior art on the
+          subject![^4][^5][^6][^7]
 
 
           [^1]: https://wiki.lix.systems/books/lix-contributors/page/flakes-feature-freeze
